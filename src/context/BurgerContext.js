e19527c1a49020f62e567b2c0bc24d43aa2da61d
NoAmd nomad <nomad.lecture@gmail.com>
2021-09-13 17:14:10 +0800

    105

--- src/context/BurgerContext.js
+++ src/context/BurgerContext.js
@@ -1,3 +1,32 @@
 import React from "react";
+import OrderPage from "../page/OrderPage";
+import ShippingPage from "../page/ShippingPage";
 
-export default React.createContext("hello!");
+const BurgerContext = React.createContext();
+
+export const BurgerStore = (props) => {
+  const [ingredients, setIngredients] = useState({
+    salad: 0,
+    cheese: 0,
+    bacon: 0,
+    meat: 0,
+  });
+
+  const addIngredient = (ingredient) => {
+    console.log("add ...");
+  };
+
+  const removeIngredient = (ingredient) => {
+    console.log("remove ...");
+  };
+
+  return (
+    <BurgerContext.Provider
+      value={(ingredients, addIngredient, removeIngredient)}
+    >
+      {props.children}
+    </BurgerContext.Provider>
+  );
+};
+
+export default BurgerContext;
